5fd9dba20fe0f200ac869fdb8d7285463a1d3a4d
Joseph <j19918015@gmail.com>
2016-02-24 19:54:21 +0800

    修改TestServlet

--- DemoWeb/src/view/TestServlet.java
+++ DemoWeb/src/view/TestServlet.java
@@ -1,8 +1,10 @@
 package view;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,7 +15,19 @@ public class TestServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
-		response.getWriter().append("<h1>served at:").append(request.getContextPath()+"</h1>");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		for(Enumeration<String> names = request.getHeaderNames();names.hasMoreElements();){
+			String name = names.nextElement();
+			out.print("<h3>"+name);
+			for(Enumeration<String> values = request.getHeaders(name);values.hasMoreElements();){
+				String value = values.nextElement();
+				out.print(value+",");
+			}
+			out.println("</h3>");
+		}
+		out.close();
 	}
 
 	protected void doPost(HttpServletRequest request,
